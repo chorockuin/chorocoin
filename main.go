c52597eedfb6d134bdf7c431e46ee3584a8701a7
chorockuin <chorockuin@gmail.com>
2021-09-10 21:08:14 +0900

    add add function

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ const (
 var templates *template.Template
 
 type home_data struct {
-	Page_title string
-	Blocks     []*blockchain.Block
+	PageTitle string
+	Blocks    []*blockchain.Block
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
@@ -26,10 +26,23 @@ func home(rw http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(rw, "home", data)
 }
 
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().Add_block(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(template_dir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(template_dir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
