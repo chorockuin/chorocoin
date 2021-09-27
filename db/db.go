526776451349b5c49444692de3b9c6e70dd7e40a
chorockuin <chorockuin@gmail.com>
2021-09-27 21:52:44 +0900

    create bolt db create data bucket create blocks bucket

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/chorockuin/chorocoin/utils"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
+		utils.HandleError(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleError(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleError(err)
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/chorockuin/chorocoin
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
